f44a2d6397db8baea150ed32e218fe38db90c4bc
Walter Morel <walter.morel.noguera@gmail.com>
2021-11-22 19:16:55 +0100

    varios

---
+++ Sección 4 Instances and Instance Attributes Use your Classes to Create Instances/main.py
@@ -0,0 +1,9 @@
+'''class Backpack:
+    def __init__(self):
+        self.items = []'''
+
+class Backpack:
+    def __init__ (self, color, size):
+        self.items = [] #se inicializará en cero
+        self.color = color
+        self.size = size
