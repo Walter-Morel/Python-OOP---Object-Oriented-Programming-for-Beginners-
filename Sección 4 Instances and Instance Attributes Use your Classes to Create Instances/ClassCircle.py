05551e2243c25bf78d3b30fc2d14e2e689cbdcc8
Walter-Morel <walter.morel.noguera@gmail.com>
2021-11-22 23:16:44 +0100

    Pasar varios argumentos a una clase

--- Sección 4 Instances and Instance Attributes Use your Classes to Create Instances/ClassCircle.py
+++ Sección 4 Instances and Instance Attributes Use your Classes to Create Instances/ClassCircle.py
@@ -1,15 +1,19 @@
-class Circle:
+'''class Circle:
 
     def __init__(self, radius, color):
         self.radius = radius
         self.color = color #digamos que queremos que todos los circulos inicien con un color en especifico, para ello en lugar de indicar el argumento 'color'
         #solamente indicamos el atributo -> ejemplo 'self.color = 'Blue'
 
-
+'''
 class Rectangle:
 
     def __init__(self, length, width):
         self.length = length
         self.width = width
+
+
+my_rectangle = Rectangle(5, 2)
+print(my_rectangle)
     
         
