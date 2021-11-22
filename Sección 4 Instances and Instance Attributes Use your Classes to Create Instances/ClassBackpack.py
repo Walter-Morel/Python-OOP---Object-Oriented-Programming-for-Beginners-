6577793149d23121eacdcefd767f56eac8cc90be
Walter-Morel <walter.morel.noguera@gmail.com>
2021-11-22 23:14:30 +0100

    Asignamos la clase que hemos creado a una variable

---
+++ Sección 4 Instances and Instance Attributes Use your Classes to Create Instances/ClassBackpack.py
@@ -0,0 +1,7 @@
+class  Backpack:
+    def __init__(self):
+        self.item = [] #inicializamos con una lista vacia
+
+
+my_backpack = Backpack() #Asignamos la clase que hemos creado a una variable, es importante poner doble parentesis luego del nombre de la clase
+print(my_backpack)
