8a5c75171dce826e67891dd752f9bd134dd1c2bf
Walter-Morel <walter.morel.noguera@gmail.com>
2021-11-22 22:58:16 +0100

    Class circle and movie

--- Sección 4 Instances and Instance Attributes Use your Classes to Create Instances/ClassCircle.py
+++ Sección 4 Instances and Instance Attributes Use your Classes to Create Instances/ClassCircle.py
@@ -10,5 +10,6 @@ class Rectangle:
 
     def __init__(self, length, width):
         self.length = length
+        self.width = width
     
         
---
+++ Sección 4 Instances and Instance Attributes Use your Classes to Create Instances/ClassMovie.py
@@ -0,0 +1,7 @@
+class Movie:
+    def __init__(self, tittle, year, language, rating):
+        self.tittle = tittle
+        self.year = year
+        self.language = language
+        self.rating = rating
+        
