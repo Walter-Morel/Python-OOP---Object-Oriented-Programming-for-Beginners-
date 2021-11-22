66af552b363680fe486cc915fa53efd76b63389d
Walter Morel <walter.morel.noguera@gmail.com>
2021-11-22 16:09:43 +0100

    Diseño de clases

---
+++ Sección 3 Classes The Blueprints of Object Oriented Programming (OOP)/main.py
@@ -0,0 +1,23 @@
+class Backpack:
+    pass
+
+#Clases para la categoria animals
+#Los nombres siempre tienen que empezar por mayusculas y tienen que ir en singular.
+class Dog:
+    pass
+class Cat:
+    pass
+class Parrot:
+    pass
+class Snake:
+    pass
+class Lizard:
+    pass
+
+#Clases para la categoria People
+class Employee:
+    pass
+class Voluntaree:
+    pass
+class Donor:
+    pass
