cfeadf9c6e04373c0872e4a8f286679e5c4574eb
Walter Morel <walter.morel.noguera@gmail.com>
2021-11-23 12:15:40 +0100

    #Fixes: #The 'age' and 'address' attribute was not well declared. #The instance 'favorite_dessert' was not declared correctly.

---
+++ Sección 4 Instances and Instance Attributes Use your Classes to Create Instances/BakerySystem_Panaderia/fix/Customer.py
@@ -0,0 +1,11 @@
+class Customer:
+    	def _init_(self, name, age, address, favorite_dessert)
+    		self.name = name
+    		self.age = age
+    		self.address = address
+    		self.favorite_dessert = favorite_dessert
+
+
+#Fixes:
+#The 'age' and 'address' attribute was not well declared.
+#The instance 'favorite_dessert' was not declared correctly.
