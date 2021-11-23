22d75d18c9f43e301eb6deecd37e04ad10b85017
Walter Morel <walter.morel.noguera@gmail.com>
2021-11-23 12:19:57 +0100

    #Fixes: #La clase Cake no esta bien declarada con la sintaxis correcta. #El init no esta bien definida. #def no declarada. #No estaba puesto el ':'

---
+++ Sección 4 Instances and Instance Attributes Use your Classes to Create Instances/BakerySystem_Panaderia/fix/Cake.py
@@ -0,0 +1,13 @@
+    class Cake:
+
+    	def __init__(self, flavor, price, quality):
+    		self.flavor = flavor
+    		self.price = price
+    		self.quality = quality
+
+
+#Fixes:
+#La clase Cake no esta bien declarada con la sintaxis correcta.
+#El init no esta bien definida.
+#def no declarada.
+#No estaba puesto el ':'
