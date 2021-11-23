b020e7cfb50095f60d4aa8dd51fe63ae15d4bfee
Walter Morel <walter.morel.noguera@gmail.com>
2021-11-23 13:11:07 +0100

    Salida en una tabla

--- Sección 5 Atributos de Clases/quadruped.py
+++ Sección 5 Atributos de Clases/quadruped.py
@@ -10,10 +10,14 @@ class   Quadruped:
         
 
 animals_quadruped = Quadruped('Dog', 'Canine', 'none', Quadruped.num_feet)
-print('Animals Quadruped is.')
+'''print('Animals Quadruped is.')
 print('Animals: {}'.format(animals_quadruped.animals))
 print('Breeds: {}'.format(animals_quadruped.breeds))
 print('Predator: {}'.format(animals_quadruped.predator))
-print('Feet: {}'.format(animals_quadruped.num_feet))
+print('Feet: {}'.format(animals_quadruped.num_feet))'''
+
+#Table output
+print('{0:10} | {1:10} | {2:10} | {3:<10}'.format('Animals', 'Breeds', 'Predator', 'Feet'))
+print('{0:10} | {1:10} | {2:10} | {3:<10}'.format(animals_quadruped.animals, animals_quadruped.breeds, animals_quadruped.predator, animals_quadruped.num_feet))
 
         
