884f3f4a730c5f45a236fbe0c2338e0cd81abdde
Walter Morel <walter.morel.noguera@gmail.com>
2021-11-23 12:11:34 +0100

    Fixes

---
+++ Sección 4 Instances and Instance Attributes Use your Classes to Create Instances/BakerySystem_Panaderia/fix/Donut.py
@@ -0,0 +1,12 @@
+class Donut:
+
+        def __ini__(self, flavor, toppings, filling, size):
+            self.flavor = flavor
+            self.toppings = toppings
+            self.filling = filling
+            self.size = size
+#Fixes:            
+#The class was not well declared, the ':' was missing.
+#The init is not defined correctly
+#The self was not set in argument
+#The instance 'Size' was not declared correctly.
