dfdbba2699f0c33515c7bf6f4f43b86367709f28
Walter Morel <walter.morel.noguera@gmail.com>
2021-11-25 12:45:04 +0100

    refactorizando codigo de la clase clinic

---
+++ Section 8 Methods/clinic.py
@@ -0,0 +1,19 @@
+#class Clinic(object):
+class Clinic:
+    
+    max_patients = 3
+    
+    def __init__(self, name):
+        self.name = name
+        self.patients = []
+        self.waiting_list = []
+    
+    def add_patient(self, patient):
+        if len(self.patients) >= Clinic.max_patients:
+            self.waiting_list.append(patient)
+        else:
+            self.patients.append(patient)
+
+my_clinic = Clinic(name=())
+my_clinic.add_patient('Danny')
+print(my_clinic.patients)
