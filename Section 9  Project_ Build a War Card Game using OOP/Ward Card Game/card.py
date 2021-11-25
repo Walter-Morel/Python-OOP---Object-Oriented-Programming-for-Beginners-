c19900a751c72b4da7c77500daa433efcb4259d1
Walter Morel <walter.morel.noguera@gmail.com>
2021-11-25 19:37:52 +0100

    definiendo clases para Ward card game

---
+++ Section 9  Project_ Build a War Card Game using OOP/Ward Card Game/card.py
@@ -0,0 +1,14 @@
+class Card:
+
+    def __init__(self, suit, value):
+        self._suit = suit
+        self._value = value
+
+    @property
+    def suit(self):
+        return self._suit
+
+    @property
+    def value(self):
+        return self._value
+        
